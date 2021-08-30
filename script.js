998448fab40c9d8d5f92376134e79bc0a22d7324
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-08-30 15:28:47 +0200

    Add an addBook function to add books to list

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
 // call elements
 const list = document.querySelector('.list'),
       author = document.querySelector('.author'),
-      title = document.querySelector('.title'),
+      title = document.querySelector('.bookTitle'),
       addBtn = document.querySelector('.addBtn');
-
-// get value from input
-const authorName = author.value,
-      titleName = title.value;
-
-// list item
-const li = document.createElement('li');
-li.className = 'item';
-
-// remove button
-li.innerHTML = `<input type="submit" value="remove" class="remove">`;
 
 // library
 let library = [];
 
+// add book
+function addBook(e) {
+  // book details
+  book = {
+    title: title.value,
+    author: author.value
+  }
+
+  // list item
+  const li = document.createElement('li');
+  li.className = 'item';
+
+  // get values
+  li.innerHTML = `&nbsp;"${book.title}"&nbsp;by&nbsp;&nbsp;${book.author}&nbsp;&nbsp;`;
+
+  // Create new link element
+  const btn = document.createElement('a');
+
+  // Add class
+  btn.className = 'remove';
+
+  // Add button
+  btn.innerHTML = '<input type="submit" value="remove" class="remove">';
+
+  // Append linkt to li
+  li.appendChild(btn);
+
+  // Append li to ul
+  list.appendChild(li);
+
+  e.preventDefault();
+}
+
+// event listener
+addBtn.addEventListener('click', addBook);
